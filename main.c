97131baf959c12a4d6fd115e6d707a60507a97fd
Steven Blair <sblair42@siu.edu>
2015-03-29 15:40:18 -0500

    Copy in code for program P1
    Copy in code for program P1 from project prompt.

--- main.c
+++ main.c
@@ -1,8 +1,64 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define DEG 20
 
 int main()
 {
-    printf("Hello world!\n");
+    int d, A[DEG][DEG], B[DEG][DEG], M[DEG*DEG][DEG*DEG];
+    int i, j, rowa, colb;
+
+    scanf("%d", &d);
+
+    for(i=0; i<d; i++)
+        for(j=0; j<d; j++)
+            scanf("%d", &A[i][j]);
+
+    for(i=0; i<d; i++)
+        for(j=0; j<d; j++)
+            scanf("%d", &B[i][j]);
+
+    for(i=0; i<d*d; i++)
+        for(j=0; j<d*d; j++)
+            M[i][j] = 0;
+
+    for(rowa=0; rowa<d; rowa++)
+        for(colb=0; colb<d; colb++)
+            for(j=0; j<d; j++)
+                if(A[rowa][j])
+                {
+                    if(M[rowa*d+colb][j*d+colb])
+                    {
+                        M[rowa*d+colb][j*d+colb] = 0;
+                    }
+                    else
+                    {
+                        M[rowa*d+colb][j*d+colb]=1;
+                    }
+                }
+
+    for(rowa=0; rowa<d; rowa++)
+        for(colb=0; colb<d; colb++)
+            for(j=0; j<d; j++)
+                if(B[j][colb])
+                {
+                    if(M[rowa*d+colb][rowa*d+j])
+                    {
+                        M[rowa*d+colb][rowa*d+j] = 0;
+                    }
+                    else
+                    {
+                        M[rowa*d+colb][rowa*d+j]=1;
+                    }
+                }
+
+    printf("\n");
+    for(i=0; i<d*d; i++)
+    {
+        for(j=0; j<d*d; j++)
+        {
+            printf("%d ", M[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
